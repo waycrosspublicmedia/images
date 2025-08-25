0381673775f8e9cd2880a8348f7cae16789da20f
mll-enh <attackgames080@gmail.com>
2025-08-25 16:59:59 -0400

    hcsjcjds

--- test/assets/characters/mball.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-	runTimer('blink',getRandomInt(1,20))
-end
-
-function onTimerCompleted(name)
-		runTimer('blink',getRandomInt(1,20))
-		triggerEvent('Play Animation','blink','gf')
-	end
